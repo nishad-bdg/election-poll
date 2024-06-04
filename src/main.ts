2ce62a53d2a8f8aa85f819eaeb19dc2cdcc465c5
Nishad Islam <nishad.nik@gmail.com>
2024-06-05 00:15:55 +0600

    crs-ts lib added

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors-ts';
 import mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: true,
   });
+
+  app.use(
+    cors({
+      origin: '#front-end-url',
+      optionsSuccessStatus: 200,
+    }),
+  );
   mongoose.set('strictPopulate', false);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
   await app.listen(3001);
 }
 bootstrap();
